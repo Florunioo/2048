50878656f5ee9a9da17279db575a4d24d80a78a6
Olivier Hubiński <olivierh133@gmail.com>
2024-06-02 17:28:20 +0200

    Tile class (init), tile colors

--- main.py
+++ main.py
@@ -28,6 +28,44 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
 
+class Tile:
+    TILE_COLORS = [
+        (237, 229, 218),  # 2 - Off White
+        (238, 225, 201),  # 4 - Light Beige
+        (243, 178, 122),  # 8 - Peach
+        (246, 150, 101),  # 16 - Light Orange
+        (247, 124, 95),   # 32 - Coral
+        (247, 95, 59),    # 64 - Red-Orange
+        (237, 208, 115),  # 128 - Light Yellow
+        (237, 204, 99),   # 256 - Mustard Yellow
+        (236, 202, 80),   # 512 - Goldenrod
+        (153, 255, 153),  # 1,024 - Light Green
+        (114, 204, 114),  # 2,048 - Green
+        (57, 168, 57),    # 4,096 - Dark Green
+        (173, 216, 230),  # 8,192 - Light Blue
+        (0, 191, 255),    # 16,384 - Deep Sky Blue
+        (0, 0, 255),      # 32,768 - Blue
+        (75, 0, 130),     # 65,536 - Indigo
+        (138, 43, 226),   # 131,072 - Violet
+        (160, 32, 240),   # 262,144 - Purple
+        (255, 20, 147),   # 524,288 - Deep Pink
+        (255, 105, 180),  # 1,048,576 - Hot Pink
+        (255, 0, 255),    # 2,097,152 - Magenta
+        (148, 0, 211),    # 4,194,304 - Dark Violet
+        (75, 0, 130),     # 8,388,608 - Indigo
+        (43, 26, 64),     # 16,777,216 - Dark Indigo
+        (18, 10, 37),     # 33,554,432 - Almost Black
+        (0, 0, 0)         # 67,108,864 - Completely Black
+    ]
+
+    def __init__(self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * TILE_WIDTH
+        self.y = row * TILE_HEIGHT
+
+
 def draw_grid(window):
     
     # draw horizontal gird lines
